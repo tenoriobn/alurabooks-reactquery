b8cf3a244cfcad070b9f6991bf327e022ec834e3
Bruno Tenório <tenoriobn@gmail.com>
2024-02-05 12:32:07 -0300

    Pagina da categoria

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import TituloPrincipal from "../../componentes/TituloPrincipal";
+import { ICategoria } from "../../interfaces/ICategoria";
+import http from "../../http";
+import { useParams } from "react-router-dom";
 
 const Categoria = () => {
+  const [categoria, setCategoria] = useState<ICategoria>();
+
+  const params = useParams();
+
+  useEffect(() => {
+    http.get<ICategoria[]>('categorias', {
+      params: {
+        slug: params.slug
+      }
+    }).then(resposta => {
+      setCategoria(resposta.data[0])
+      console.log(resposta.data);
+    })
+  }, [params.slug])
+
   return (
     <section>
-      <TituloPrincipal texto="Categoria" />
+      <TituloPrincipal texto={categoria?.nome ?? ''} />
     </section>
   );
 }
