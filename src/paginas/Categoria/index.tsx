3ba2d4d886a3c97107e81ea514a82b3202c7ae50
Bruno Tenório <tenoriobn@gmail.com>
2024-02-06 11:14:45 -0300

    exibindo o loader condicionalmente

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -7,23 +7,29 @@ import Loader from "../../componentes/Loader";
 
 const Categoria = () => {
   const [categoria, setCategoria] = useState<ICategoria>();
+  const [estaCarregando, setEstaCarregando] = useState(true);
 
   const params = useParams();
 
   useEffect(() => {
+    setEstaCarregando(true);
     http.get<ICategoria[]>('categorias', {
       params: {
         slug: params.slug
       }
     }).then(resposta => {
+      setEstaCarregando(false);
       setCategoria(resposta.data[0])
     })
   }, [params.slug])
 
+  if (estaCarregando) {
+    return <Loader />
+  }
+
   return (
     <section>
       <TituloPrincipal texto={categoria?.nome ?? ''} />
-      <Loader />
     </section>
   );
 }
