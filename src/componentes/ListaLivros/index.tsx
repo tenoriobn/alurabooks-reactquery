aa4952d8e558082d8edd42131af434359ec9d9af
Bruno Tenório <tenoriobn@gmail.com>
2024-02-07 11:39:49 -0300

    estilo da lista

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,14 +1,19 @@
+import { useQuery } from "react-query";
 import { ICategoria } from "../../interfaces/ICategoria";
+import { obterProdutosDaCategoria } from "../../http";
+import CardLivro from "../CardLivro";
 
 interface ListaLivrosProps {
   categoria: ICategoria
 }
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
-  console.log(categoria)
-  return (
-    <></>
-  )
+  const {data: produtos} = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
+  
+  console.log(produtos)
+  return <>
+    {produtos?.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+  </>
 }
 
 export default ListaLivros;
